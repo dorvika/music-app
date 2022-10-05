1a7e86a914579894f2a2621d8a3d442b443c4ebb
Viktoriia Doroshenko <dorvika2@gmail.com>
2022-10-05 19:15:36 +0300

    music app finished project

---
+++ src/pages/ArtistDetails.jsx
@@ -0,0 +1,35 @@
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
+import { useGetArtistDetailsQuery } from "../redux/services/shazamCore";
+
+const ArtistDetails = () => {
+  const { id: artistId } = useParams();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const {
+    data: artistData,
+    isFetching: isFetchingArtistDetails,
+    error,
+  } = useGetArtistDetailsQuery(artistId);
+
+  if (isFetchingArtistDetails) return <Loader title="Loading artist details" />;
+
+  if (error) return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <DetailsHeader artistId={artistId} artistData={artistData} />
+      <div className="mb-10">
+        <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
+      </div>
+      <RelatedSongs
+        data={Object.values(artistData?.songs)}
+        artistId={artistId}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+      />
+    </div>
+  );
+};
+
+export default ArtistDetails;
